5bfc0f2f9742b994b7c70cf73baf21371e4eb068
Ryan Chen <rchen73@ucsc.edu>
2021-04-16 14:04:17 -0700

    Fourth Commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,14 +1,12 @@
-const { Phaser } = require("../../lib/phaser");
-
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
         // add object to existing scene
-        scene.add.existing(this);
-        this.isFiring = false;
-        this.moveSpeed = 2;
+        scene.add.existing(this); // add to existing, displayList, updateList
+        this.isFiring = false;    // track rocket's firing status
+        this.moveSpeed = 2;       // pixels per frame
     }
 
     update() {
@@ -27,6 +25,19 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         // if fired, move up
-        if(this.isFiring && this.y >= borderUISize)
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    // reset rocket to ground
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
